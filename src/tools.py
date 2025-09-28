766d662ad4d3e7bcfbbd736fcc6fb593f7bdfad5
ronak-navadiya <ronaknavadiya123@gmail.com>
2025-09-27 22:45:51 -0400

    get all expenses tool

--- src/tools.py
+++ src/tools.py
@@ -1,15 +1,18 @@
 import os
 import sqlite3
-from db import init_db
+from .db import init_db
+from pathlib import Path
 
 # Sqlite DB Path
-DB_PATH = os.path.join(os.path.dirname(__file__), "expense.db")
+# DB_PATH = os.path.join(os.path.dirname(__file__), "expense.db")
+DB_PATH = Path(__file__).parent.parent / "expense.db"
+# print("DB_PATH", DB_PATH)
 
 # Init DB if not exists 
 init_db(DB_PATH)
 
 def register_tools(mcp):
-    """Register all expense tracking tools with the MCP server."""
+    """Register all expense tracking tools with the MCP server"""
     
     @mcp.tool()
     def add_expense(date, amount, category, subcategory="", note=""):
@@ -20,3 +23,15 @@ def register_tools(mcp):
                 (date, amount, category, subcategory, note)
             )
             return {"status": "ok", "id": cur.lastrowid}
+
+    @mcp.tool()
+    def get_all_expenses():
+        """Retrieve all expenses from the database"""
+        with sqlite3.connect(DB_PATH) as conn:
+            conn.row_factory = sqlite3.Row
+            cur = conn.execute("SELECT * FROM expenses ORDER BY date DESC")
+            # print("curr ->" , cur)
+            # for row in cur.fetchall():
+            #     print(row) 
+            expenses = [dict(row) for row in cur.fetchall()]
+            return {"status":"Ok", "expenses":expenses}
